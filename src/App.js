7c8a6126ba2e76a9657989e8d9650a83afe02f40
Ryan <ryan-fontaine@hotmail.fr>
2023-04-27 15:22:30 +0200

    Create a blog with React JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      isInputValid: false
+    };
+  }
+  
+  handleChange = (event) => {
+    const input = event.target.value;
+    const isValid = input.length >= 60;
+    this.setState({ text: input, isInputValid: isValid });
+  }
+  
+  renderText = () => {
+    return <p>{this.state.text}</p>;
+  }
+  
+  handleSubmit = () => {
+    alert(`Article de blog soumis avec succès : "${this.state.text}" (Nombre de caractères : ${this.state.text.length})`);
+  }
+  
+  render() {
+    let message = '';
+    let messageClass = '';
+    if (this.state.text.length < 60) {
+      message = 'Votre message doit comporter au moins 60 caractères.';
+    } else {
+      message = 'Votre message a au moins 60 caractères, il peut être envoyé.';
+      messageClass = 'green';
+    }
+    return (
+      <div className="App">
+        <div className="header">Editeur d'article de blog - 
+        <span className="name yellow">&nbsp;Ryan, Antoine, Richard</span>
+        </div>
+        <div className={`alert ${messageClass}`}>{message}</div>
+        <textarea className="textarea" value={this.state.text} onChange={this.handleChange} placeholder="Entrez votre message ici s'il vous plait..." />
+        <div className="counter">Nombre de caractères : {this.state.text.length}/1000 caractères</div>
+        <div className="submit">
+          <button disabled={!this.state.isInputValid} onClick={this.handleSubmit}>Envoyer</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
